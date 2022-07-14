2772c8f5d72c078485d71ae7ebca710828144588
rin4046 <72665456+rin4046@users.noreply.github.com>
2022-07-15 05:12:26 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export DENO_INSTALL="./.deno"
-curl -fsSL https://deno.land/x/install/install.sh | sh
+curl -fsSL https://deno.land/x/install/install.sh | sh -s v1.22.0
 
 ./.deno/bin/deno run --unstable --allow-net --allow-read --allow-write bundle.ts
 npx esbuild --minify ./static/main.js --outdir=./static --allow-overwrite
